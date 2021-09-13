d622d25d9798b32d69fa62ce9514ea0effdc1e8c
Zylern <47510180+Zylern@users.noreply.github.com>
2021-09-13 19:16:39 +0530

    Add files via upload

---
+++ bot/funcn.py
@@ -0,0 +1,150 @@
+#    This file is part of the CompressorQueue distribution.
+#    Copyright (c) 2021 Danish_00
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/1Danish-00/CompressorQueue/blob/main/License> .
+
+from . import *
+from .config import *
+
+WORKING = []
+QUEUE = {}
+OK = {}
+
+uptime = dt.now()
+os.system(f"wget {THUMB} -O thumb.jpg")
+
+if not os.path.isdir("downloads/"):
+    os.mkdir("downloads/")
+if not os.path.isdir("encode/"):
+    os.mkdir("encode/")
+if not os.path.isdir("thumb/"):
+    os.mkdir("thumb/")
+
+
+def stdr(seconds: int) -> str:
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    if len(str(minutes)) == 1:
+        minutes = "0" + str(minutes)
+    if len(str(hours)) == 1:
+        hours = "0" + str(hours)
+    if len(str(seconds)) == 1:
+        seconds = "0" + str(seconds)
+    dur = (
+        ((str(hours) + ":") if hours else "00:")
+        + ((str(minutes) + ":") if minutes else "00:")
+        + ((str(seconds)) if seconds else "")
+    )
+    return dur
+
+
+def ts(milliseconds: int) -> str:
+    seconds, milliseconds = divmod(int(milliseconds), 1000)
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    tmp = (
+        ((str(days) + "d, ") if days else "")
+        + ((str(hours) + "h, ") if hours else "")
+        + ((str(minutes) + "m, ") if minutes else "")
+        + ((str(seconds) + "s, ") if seconds else "")
+        + ((str(milliseconds) + "ms, ") if milliseconds else "")
+    )
+    return tmp[:-2]
+
+
+def hbs(size):
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "B", 1: "K", 2: "M", 3: "G", 4: "T", 5: "P"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
+
+async def progress(current, total, event, start, type_of_ps, file=None):
+    now = time.time()
+    diff = now - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        time_to_completion = round((total - current) / speed) * 1000
+        progress_str = "`[{0}{1}] {2}%`\n\n".format(
+            "".join(["●" for i in range(math.floor(percentage / 5))]),
+            "".join(["○" for i in range(20 - math.floor(percentage / 5))]),
+            round(percentage, 2),
+        )
+        tmp = (
+            progress_str
+            + "`{0} of {1}`\n\n`✦ Speed: {2}/s`\n\n`✦ ETA: {3}`\n\n".format(
+                hbs(current),
+                hbs(total),
+                hbs(speed),
+                ts(time_to_completion),
+            )
+        )
+        if file:
+            await event.edit(
+                "`✦ {}`\n\n`File Name: {}`\n\n{}".format(type_of_ps, file, tmp)
+            )
+        else:
+            await event.edit("`✦ {}`\n\n{}".format(type_of_ps, tmp))
+
+
+async def info(file, event):
+    process = subprocess.Popen(
+        ["mediainfo", file, "--Output=HTML"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+    )
+    stdout, stderr = process.communicate()
+    out = stdout.decode()
+    client = TelegraphPoster(use_api=True)
+    client.create_api_token("Mediainfo")
+    page = client.post(
+        title="Mediainfo",
+        author=((await event.client.get_me()).first_name),
+        author_url=f"https://t.me/{((await event.client.get_me()).username)}",
+        text=out,
+    )
+    return page["url"]
+
+
+def code(data):
+    OK.update({len(OK): data})
+    return str(len(OK) - 1)
+
+
+def decode(key):
+    if OK.get(int(key)):
+        return OK[int(key)]
+    return
+
+
+async def skip(e):
+    wah = e.pattern_match.group(1).decode("UTF-8")
+    wh = decode(wah)
+    out, dl, id = wh.split(";")
+    try:
+        if QUEUE.get(int(id)):
+            WORKING.clear()
+            QUEUE.pop(int(id))
+        await e.delete()
+        os.remove(dl)
+        os.remove(out)
+    except BaseException:
+        pass
+    return
