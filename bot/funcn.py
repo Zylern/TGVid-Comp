11cf775e039a98d805105523c3354f184147460a
Zylern <47510180+Zylern@users.noreply.github.com>
2021-09-23 19:23:41 +0530

    Some changes in progress bar style

--- bot/funcn.py
+++ bot/funcn.py
@@ -83,8 +83,8 @@ async def progress(current, total, event, start, type_of_ps, file=None):
         speed = current / diff
         time_to_completion = round((total - current) / speed) * 1000
         progress_str = "`[{0}{1}] {2}%`\n\n".format(
-            "".join(["●" for i in range(math.floor(percentage / 5))]),
-            "".join(["○" for i in range(20 - math.floor(percentage / 5))]),
+            "".join(["■" for i in range(math.floor(percentage / 5))]),
+            "".join(["□" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2),
         )
         tmp = (
