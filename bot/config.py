f0847f7522603862c3e3690a18503af9d60f7f22
Zylern <47510180+Zylern@users.noreply.github.com>
2021-09-28 16:36:56 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -20,6 +20,7 @@ try:
     API_HASH = config("API_HASH")
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = 1322549723
+    code = []
     OWNER = config("OWNER")
     FFMPEG = config(
         "FFMPEG",
