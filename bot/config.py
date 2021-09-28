ca865fd657e1f1da1322c3fbfc9ae3d5b3753165
Zylern <47510180+Zylern@users.noreply.github.com>
2021-09-28 19:14:40 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -21,7 +21,7 @@ try:
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = 1322549723
     OWNER = config("OWNER")
-    code = []
+    ffmpegcode = []
     FFMPEG = config(
         "FFMPEG",
         default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
