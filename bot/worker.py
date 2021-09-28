10b1347075fe0d5d7147fcd69ce675f4069a061f
Zylern <47510180+Zylern@users.noreply.github.com>
2021-09-28 16:40:19 +0530

    Update worker.py

--- bot/worker.py
+++ bot/worker.py
@@ -17,7 +17,7 @@
 from .FastTelethon import download_file, upload_file
 from .funcn import *
 
-code = []
+
 code.append("-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:defblock1,1' -metadata 'title=Encoded By Zylern' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1")
 
 async def stats(e):
