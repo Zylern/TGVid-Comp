0b8d8485f66f536391522217f30eddb1edc72ad2
Zylern <47510180+Zylern@users.noreply.github.com>
2022-04-06 09:41:13 +0530

    Fixes in sysinfo

--- bot/funcn.py
+++ bot/funcn.py
@@ -2,14 +2,14 @@
 #    Copyright (c) 2021 Danish_00
 #    Script Improved by Zylern
 
-
 from . import *
 from .config import *
 from .worker import *
 from asyncio import create_subprocess_shell as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
-import psutil, os, signal
+import psutil, os, signal, sys, platform, sysconfig
 from bot import ffmpegcode, LOG_FILE_NAME
+from psutil import disk_usage, cpu_percent, virtual_memory, Process as psprocess
 
 WORKING = []
 QUEUE = {}
@@ -114,20 +114,19 @@ async def test(event):
         await event.reply("**Install speedtest-cli**")
 
 
-async def sysinfo(event):
-    try:
-        zyl = "neofetch --stdout"
-        fetch = await asyncrunapp(
-            zyl,
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-        stdout, stderr = await fetch.communicate()
-        result = str(stdout.decode().strip()) \
-            + str(stderr.decode().strip())
-        await event.reply("**" + result + "**")
-    except FileNotFoundError:
-        await event.reply("**Install neofetch first**")
+async def sysinfo(e):
+    if str(e.sender_id) not in OWNER and event.sender_id !=DEV:
+        return
+    total, used, free, disk= disk_usage('/')
+    total = hbs(total)
+    free = hbs(free)
+    memory = virtual_memory()
+    mem_p = memory.percent
+    mem_t = hbs(memory.total)
+    mem_a = hbs(memory.available)
+    mem_u = hbs(memory.used)
+    await e.reply(f"**OS:** {platform.system()}\n**Version:** {platform.release()}\n**Arch:** {platform.architecture()}\n**Total Disk Space:** {total}\n**Free:** {free}\n**Memory Total:** {mem_t}\n**Memory Free:** {mem_a}\n**Memory Used:** {mem_u}\n")
+    return
 
 
 async def info(file, event):
