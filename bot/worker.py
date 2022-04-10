9af017174fc431704f34248e561f7332188c5119
Zylern <coolzylern@gmail.com>
2022-04-10 14:17:34 +0530

    Bug Fixed In Upload

--- bot/worker.py
+++ bot/worker.py
@@ -102,19 +102,16 @@ async def dl_link(event):
     eees = dt.now()
     x = dtime
     xx = ts(int((ees - es).seconds) * 1000)
-    xxx = ts(int((eees - ees).seconds) * 1000)
+    xxxx = ts(int((eees - ees).seconds) * 1000)
     a1 = await info(dl, xxx)
     a2 = await info(out, xxx)
-#    dk = f"<b>File Name:</b> {newFile}\n\n<b>Original File Size:</b> {hbs(org)}\n<b>Encoded File Size:</b> {hbs(com)}\n<b>Encoded Percentage:</b> {per}\n\n<b>Get Mediainfo Here:</b> <a href='{a1}'>Before</a>/<a href='{a2}'>After</a>"
-    ds = await e.client.send_file(
-        e.chat_id, file=ok, force_document=True, link_preview=False, thumb=thum, parse_mode="html"
+    dk = f"<b>File Name:</b> {newFile}\n\n<b>Original File Size:</b> {hbs(org)}\n<b>Encoded File Size:</b> {hbs(com)}\n<b>Encoded Percentage:</b> {per}\n\n<b>Get Mediainfo Here:</b> <a href='{a1}'>Before</a>/<a href='{a2}'>After</a>\n\n<i>Downloaded in {x}\nEncoded in {xx}\nUploaded in {xxxx}</i>"
+    ds = await event.client.send_file(
+        event.chat_id, file=ok, caption=dk, force_document=True, link_preview=False, thumb=thum, parse_mode="html"
     )
     os.remove(dl)
     os.remove(out)
     WORKING.clear()
-
-
-
 
 
 async def encod(event):
